6dcfbb2c87e319a717d78bb15c57180250b520e9
YamilRA <jyra010920@gmail.com>
2026-09-24 22:37:34 -0600

    clase persona

--- Persona.cs
+++ Persona.cs
@@ -8,6 +8,14 @@ namespace MyApp
 {
     internal class Persona
     {
-
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public String Telefono { get; set; }
+        public Persona(int id, String nombre, String telefono)
+        {
+            Id = id;
+            Nombre = nombre;
+            Telefono = telefono;
+        }
     }
 }
